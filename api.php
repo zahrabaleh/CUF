f169c7c9fda85ddba49052f7e0143e0bb67ff452
zahrabaleh <69067268+zahrabaleh@users.noreply.github.com>
2020-08-01 09:14:39 +0430

    Create api.php

---
+++ api.php
@@ -0,0 +1,39 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://companyurlfinder.com/cuf?companyName=microsoft&api_key=YOUR_API_KEY",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_POSTFIELDS => "",
+	CURLOPT_HTTPHEADER => array(
+		"content-type: application/json"
+	),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
+
+
+
+
+
+
+
+
+
+?>
